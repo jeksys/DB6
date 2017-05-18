e7b09105a8e03f6b2c6b0835127e889a1b0b7f37
Eugene Yagrushkin <yagrushkin@gmail.com>
2017-05-18 15:19:00 -0700

    Create SwiftyConfiguration.h

---
+++ DB6/SwiftyConfiguration.h
@@ -0,0 +1,17 @@
+//
+//  DB6.h
+//  DB6
+//
+//  Created by Eugene Yagrushkin on 2016/07/05.
+//  Copyright © 2017 Eugene Yagrushkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DB6.
+FOUNDATION_EXPORT double DB6VersionNumber;
+
+//! Project version string for DB6.
+FOUNDATION_EXPORT const unsigned char DB6VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DB6/PublicHeader.h>
